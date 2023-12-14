007e02ec5503836139a2e6eec74f49936fe352db
leo <nascimentoleo899@gmail.com>
2023-12-14 20:14:26 -0300

    implementa camada api/controller

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaRestController.java
@@ -35,6 +35,14 @@ public class TarefaRestController implements TarefaAPI {
 		return new TarefaDetalhadoResponse(tarefa);
 	}
 
+	@Override
+	public void incrementaPomodoroTarefa(String token, UUID idTarefa) {
+		log.info("[inicia] TarefaRestController - incrementaPomodoroTarefa");
+		String email = getUsuarioByToken(token);
+		tarefaService.incrementaPomodoroTarefa(email, idTarefa);
+		log.info("[finaliza] TarefaRestController - incrementaPomodoroTarefa");
+	}
+
 	private String getUsuarioByToken(String token) {
 		log.debug("[token] {}", token);
 		String usuario = tokenService.getUsuarioByBearerToken(token).orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, token));
