96cce60cb7c0f06def655086de3c585d3b51c6fd
leo <nascimentoleo899@gmail.com>
2023-12-18 16:34:20 -0300

    refatora metodos dos testes unitarios

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -27,25 +27,21 @@ class UsuarioApplicationServiceTest {
     private UsuarioRepository usuarioRepository;
 
     @Test
-    public void alterStatusParaFocoComSucesso(){
+    public void deveAlterarStatusParaPausaLongaComSucesso(){
         Usuario usuario = DataHelper.createUsuarioComPausaCurta();
-
         when(usuarioRepository.buscaUsuarioPorEmail(anyString())).thenReturn(usuario);
         usuarioApplicationService.alteraStatusParaPausaLonga(usuario.getEmail(), usuario.getIdUsuario());
-
         verify(usuarioRepository, times(1)).salva(usuario);
         assertEquals(StatusUsuario.PAUSA_LONGA, usuario.getStatus());
     }
 
     @Test
-    public void deveRetornExceptionAoMudarStatusParaPausaLongaComIdInvalido(){
+    public void deveRetornarExceptionAoMudarStatusParaPausaLongaComIdInvalido(){
         UUID idInvalido = UUID.randomUUID();
         Usuario usuario = DataHelper.createUsuarioComPausaCurta();
-
         when(usuarioRepository.buscaUsuarioPorEmail(anyString())).thenReturn(usuario);
         APIException ex = assertThrows(APIException.class, () -> usuarioApplicationService
                 .alteraStatusParaPausaLonga(usuario.getEmail(), idInvalido));
-
         assertEquals(HttpStatus.UNAUTHORIZED, ex.getStatusException());
         assertEquals("credencial de autenticação não é valida", ex.getMessage());
     }
