f116df3c9a87c546af513a18a371155daec18198
tamiresrigoti <tamiresrigoti@gmail.com>
2023-12-18 19:24:49 -0300

    implementa testes unitários, ok!

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -2,9 +2,14 @@ package dev.wakandaacademy.produdoro.tarefa.application.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -13,36 +18,85 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.handler.APIException;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusAtivacaoTarefa;
+import dev.wakandaacademy.produdoro.tarefa.domain.StatusTarefa;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 
 @ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
 
-    //	@Autowired
-    @InjectMocks
-    TarefaApplicationService tarefaApplicationService;
+	// @Autowired
+	@InjectMocks
+	TarefaApplicationService tarefaApplicationService;
 
-    //	@MockBean
-    @Mock
-    TarefaRepository tarefaRepository;
+	// @MockBean
+	@Mock
+	TarefaRepository tarefaRepository;
 
-    @Test
-    void deveRetornarIdTarefaNovaCriada() {
-        TarefaRequest request = getTarefaRequest();
-        when(tarefaRepository.salva(any())).thenReturn(new Tarefa(request));
-  
-        TarefaIdResponse response = tarefaApplicationService.criaNovaTarefa(request);
+	@Mock
+	UsuarioRepository usuarioRepository;
 
-        assertNotNull(response);
-        assertEquals(TarefaIdResponse.class, response.getClass());
-        assertEquals(UUID.class, response.getIdTarefa().getClass());
-    }
+	@Test
+	void deveRetornarIdTarefaNovaCriada() {
+		TarefaRequest request = getTarefaRequest();
+		when(tarefaRepository.salva(any())).thenReturn(new Tarefa(request));
 
-    public TarefaRequest getTarefaRequest() {
-        TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
-        return request;
-    }
+		TarefaIdResponse response = tarefaApplicationService.criaNovaTarefa(request);
+
+		assertNotNull(response);
+		assertEquals(TarefaIdResponse.class, response.getClass());
+		assertEquals(UUID.class, response.getIdTarefa().getClass());
+	}
+
+	public TarefaRequest getTarefaRequest() {
+		TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
+		return request;
+	}
+
+	@Test
+	void mudaStatusParaConcluida_PositiveScenario() {
+		// Arrange
+		UUID usuarioId = UUID.fromString("a713162f-20a9-4db9-a85b-90cd51ab18f4");
+		UUID tarefaId = UUID.fromString("06fb5521-9d5a-461a-82fb-e67e3bedc6eb");
+
+		Usuario usuario = DataHelper.createUsuario();
+		Tarefa tarefa = DataHelper.createTarefa();
+
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.of(tarefa));
+
+		// Act
+		tarefaApplicationService.mudaStatusParaConcluida("email@email.com", tarefaId);
+
+		// Assert
+		assertEquals(StatusTarefa.CONCLUIDA, tarefa.getStatus());
+		verify(tarefaRepository, times(1)).salva(tarefa);
+	}
+
+	@Test
+	void mudaStatusParaConcluida_NegativeScenario_TarefaNaoEncontrada() {
+		// Arrange
+		UUID usuarioId = UUID.fromString("a713162f-20a9-4db9-a85b-90cd51ab18f4");
+		UUID tarefaId = UUID.fromString("06fb5521-9d5a-461a-82fb-e67e3bedc6eb");
+
+		Usuario usuario = DataHelper.createUsuario();
+
+		when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+		when(tarefaRepository.buscaTarefaPorId(any())).thenReturn(Optional.empty());
+
+		// Act and Assert
+		assertThrows(APIException.class, () -> {
+			tarefaApplicationService.mudaStatusParaConcluida("email@email.com", tarefaId);
+		});
+
+		// Verify
+		verify(tarefaRepository, never()).salva(any());
+	}
 }
